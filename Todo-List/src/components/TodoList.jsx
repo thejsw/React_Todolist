3a28a856b73a0e106a87ece9d7b09d79fcbc87c2
thejsw <jswtncapetnc@gmail.com>
2023-04-13 18:26:12 +0900

    TodoList 컴포넌트 수정

--- Todo-List/src/components/TodoList.jsx
+++ Todo-List/src/components/TodoList.jsx
@@ -4,10 +4,7 @@ import TodoItem from './TodoItem';
 import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
-  flex: 1;
   padding: 20px 32px;
-  padding-bottom: 48px;
-  overflow-y: auto;
 `;
 
 function TodoList() {
